e7995d7a35e53447cedb2c52bef85839e4ce50ad
Bryan <bryanj1234@gmail.com>
2024-12-09 05:57:02 -0500

    nothing to see

--- brylibpromptandstructure.py
+++ brylibpromptandstructure.py
@@ -14,6 +14,7 @@ class GenericResponseStructure(BaseModel):
     prompt_7: str
     prompt_8: str
     prompt_9: str
+
     response_to_prompt_1: str
     response_to_prompt_2: str
     response_to_prompt_3: str
@@ -23,6 +24,8 @@ class GenericResponseStructure(BaseModel):
     response_to_prompt_7: str
     response_to_prompt_8: str
     response_to_prompt_9: str
+
+    added_prompt_explanation: str
     additional_information: str
 
 def generic_prompts_list_to_prompt(prompt_list):
@@ -38,6 +41,8 @@ def generic_prompts_list_to_prompt(prompt_list):
 
     final_prompt += "\n"
     final_prompt += "When you are structuring the output, please populate the fields which start with \"prompt_\" with the corresponding task prompt, and populate the fields which start with \"response_to_prompt_\" with your corresponding response. "
+    final_prompt += "If there are additional interesting questions which I did not ask, please add them into the unused \"prompt_\" fields, and add your responses in corresponding \"response_to_prompt_\" fields. "
+    final_prompt += "If you choose to populate the \"prompt_\" fields with additional prompts which I didn't send you, then please explain why using the \"added_prompt_explanation\" field. If you did not add any additional prompts, then populate \"added_prompt_explanation\" field with the string \"NO_ADDITIONAL\". "
     final_prompt += "Finally, please evaluate how you did on these tasks, and add your thoughts to the \"additional_information\" field."
 
     return final_prompt
